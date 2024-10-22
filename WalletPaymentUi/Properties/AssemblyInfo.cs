997391ab39b94d1b573e68cc196dcb3677eab1bb
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-23 00:33:42 +0330

    - Update AssmblyInfo Project

--- WalletPaymentUi/Properties/AssemblyInfo.cs
+++ WalletPaymentUi/Properties/AssemblyInfo.cs
@@ -8,12 +8,12 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WalletPaymentUi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Designing a payment wallet in WPF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AminMirzaeiOne")]
 [assembly: AssemblyProduct("WalletPaymentUi")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("AminMirzaeiOne")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
